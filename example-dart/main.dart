de01cfd6ee02b89d953ae4b988f22cb7575c705d
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2021-06-23 22:09:05 -0300

    Primeiro projeto - CounterApp

--- example-dart/main.dart
+++ example-dart/main.dart
@@ -1,7 +1,12 @@
 void main() {
-    printHelloWorld(message: "Deu certo!");
+  var minhaClasse = MinhaClasse();
+  printHelloWorld(message: minhaClasse.message);
 }
 
 void printHelloWorld({required String message}){
   print(message.replaceAll("Deu", "Sim, Deu"));
+}
+
+class MinhaClasse {
+  var message = "Deu certo";
 }
