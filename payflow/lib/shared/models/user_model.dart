6f92aa72b952fb715cf0a073d3465daa9c7bd113
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2021-07-24 12:47:18 -0300

    Splashscreen e login

---
+++ payflow/lib/shared/models/user_model.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class UserModel {
+  final String name;
+  final String? photoUrl;
+
+  UserModel({required this.name, required this.photoUrl});
+
+  Map<String, dynamic> toMap() =>{
+    "name" : name,
+    "photoUrl" : photoUrl
+  };
+
+  String toJson() => jsonEncode(toMap());
+
+  factory UserModel.fromMap(Map<String, dynamic> map){
+    return UserModel(name: map['name'], photoUrl: map['photoUrl']);
+  }
+
+  factory UserModel.fromJson(String json) 
+    => UserModel.fromJson(jsonEncode(json));
+}
